ce32b82d264af770702094a63f80749012e377de
Okiichinpo <dean.c.garcia@gmail.com>
2020-05-12 00:20:57 -0700

    Began laying some of the ground work for the Preemptive sets and parallelized a small portion of the markup code

--- sudokuBuilder.c
+++ sudokuBuilder.c
@@ -14,7 +14,7 @@ using namespace std;
 
 
 int const grid_size = 9;
-int clue_count = 17;
+int clue_count = 53;
 int runtime_threshold = 100;
 int const experiment_ephochs = 100;
 
@@ -36,14 +36,17 @@ public:
 	}
 };
 
-class Board {
+class Board 
+{
 public:
-	int valid = 1;
+	int valid;
 	Cell cells[grid_size][grid_size];
 	int rows[grid_size][grid_size]; //rows[rowID][markup] markup = {0,0,1,1} -> 3,4 are valid entries
 	int cols[grid_size][grid_size]; //cols[colID][markup] 
 	int boxes[grid_size][grid_size]; //boxes[boxID][markup]
-	Board() {
+	Board() 
+	{
+		setValid();
 		for (int i = 0; i < grid_size; i++) {
 			for (int j = 0; j < grid_size; j++) {
 				Cell newCell(0, i, j);
@@ -62,53 +65,99 @@ public:
 			}
 		}
 	}
+
+	void setValid()
+	{
+		valid = 1;
+	}
+
+	// void preemptive_sets()
+	// {
+	// 	for (int i = 0; i < grid_size; i++) 
+	// 	{
+	// 		for (int j = 0; j < grid_size; j++) 
+	// 		{
+	// 			if(cells[i][j] == 0)
+	// 			{
+					//int markup_count = cell.markup[0];
+					//for (k = 1; k <= markup_count; k++)
+					//{
+	// 				// *******Instead of iterating through each cell just go row by row looking for sets
+	// 				// *********then go column by column loooking for sets 
+	// 				// *********then go square to square looking for sets
+	// 				Cell markup = p_get_markup(i, j);
+	// 				for (int i = 0; i < grid_size + 1; i++) 
+	// 				{
+	// 					board.rows[i][j]
+	// 					board.cols[col][i]
+	// 					board.cols[col][i]
+	// 					int box = board.get_box_id(row, col);
+	// 					board.boxes[box][i]
+	// 				}
+		
+	// 				// get all row markups
+	// 				// get all column markups 
+	// 				// get all box markups
+	// 				// for cell with row and col
+
+	// 				// get all the empty cells of a row.
+	// 				// union them together is it prememptive no then reduce the amount of cells yo uunion
+	// 				// 
+
+	// 				// TODO: now that you have all rows columns and box markups
+	// 				// compare the size which is the markup array[0] 0 index. If we have a match for
+	// 				// this 
+	// 				// start with the cell that has the largest markup
+	// 				// then check to see if other cells have the same numbers in their markup 
+	// 				// they must only have the numbers from the largest markup cell
+	// 				// then check to see if there are x number of cells with only these numbers
+	// 				// where x is the amount of numbers in the largest markup cell. once doing that you 
+	// 				// can eliminate all other instances of those distinct numbers in other cell markups that 
+	// 				// are not apart of the prememptive set in the same row/column/box. You can only eliminate from 
+	// 				// wherever you checked like if you look for preemptive set in a row then you can only eliminate 
+	// 				// eiliminate from the row
+	// 			}
+	// 		}
+	// 	}
+	// }
+
 	int get_box_id(int row, int col) {
 		int box_size = sqrt(grid_size);
 		int box_row = row / box_size;
 		int box_col = col / box_size;
 		return box_col + box_row * box_size;
 	}
+
 	Cell getCell(int id) {
 		int col = id % grid_size;
 		int row = id / grid_size;
 
 		return cells[col][row];
 	}
-	/*int* get_row_markup(int row) {
-		return rows[row];
-	}
-	int* get_col_markup(int col) {
-		return cols[col];
-	}
-	int* get_box_markup(int row, int col) {
-		int box_id = get_box_id(row, col);
-		return boxes[box_id];
-	}*/
 	Cell get_markup(int row, int col) {
 		Cell cell = cells[col][row];
 		//int markup[grid_size + 1];
 		int box_id = get_box_id(row, col);
 		int markup_size = 0;
+
 		for (int i = 1; i < grid_size + 1; i++) {
-			//printf("%d\n", i);
 			if (rows[row][i - 1] == 1 && cols[col][i - 1] == 1 && boxes[box_id][i - 1] == 1 && cells[col][row].value == 0)
 			{
-				//markup[i] = 1;
-
-				//cell.markup[i] = 1;
 				cells[col][row].markup[i] = 1;
 				markup_size++;
 			}
 			else {
-				//cell.markup[i] = 0;
 				cells[col][row].markup[i] = 0;
-				//markup[i] = 0;
 			}
 		}
-		//markup[0] = markup_size;
 		cells[col][row].markup[0] = markup_size;
 		return cells[col][row];
 	}
+
+	Cell p_get_markup(int row, int col) {
+		return cells[col][row];
+	}
+
 	int get_kth(int* markup, int k) {
 		int count = 0;
 		int index = 0;
@@ -124,7 +173,6 @@ public:
 
 	void add_value(int value, int row, int col) {
 		int box_id = get_box_id(row, col);
-		//printf("%d\n", value);
 		cells[col][row].value = value;
 		rows[row][value - 1] = 0;
 		cols[col][value - 1] = 0;
@@ -138,6 +186,39 @@ public:
 		cols[col][value - 1] = 1;
 		boxes[box_id][value - 1] = 1;
 	}
+ 
+	void set_markup()
+	{
+		int i, j, k;
+		int markup_size;
+		#pragma omp parallel private(i, j, k)
+		{
+			#pragma omp for reduction(+:markup_size), schedule(static)
+			for (j = 0; j < grid_size; j++)
+			{
+				for (i = 0; i < grid_size; i++)
+				{
+					if (cells[i][j].value == 0)
+					{
+						int box_id = get_box_id(row, col);
+						markup_size = 0;
+						for (k = 1; k < grid_size + 1; k++) 
+						{
+							if (rows[j][k - 1] == 1 && cols[i][k - 1] == 1 && boxes[box_id][k - 1] == 1 && cells[i][j].value == 0)
+							{
+								cells[i][j].markup[k] = 1;
+								markup_size++;
+							}
+							else {
+								cells[i][j].markup[k] = 0;
+							}
+						}
+						cells[i][j].markup[0] = markup_size;
+					}
+				}
+			}
+		}
+	}
 
 	void generate_markup() {
 		for (int i = 0; i < grid_size; i++) {
@@ -145,10 +226,10 @@ public:
 				if (cells[i][j].value != 0) add_value(cells[i][j].value, j, i);
 			}
 		}
+		set_markup();
 	}
 
 	void check_forced_cells() {
-		//generate_markup();
 		int forced_count = 0;
 		for (int i = 0; i < grid_size; i++) {
 			for (int j = 0; j < grid_size; j++) {
@@ -159,13 +240,12 @@ public:
 				}
 			}
 		}
-		//if (forced_count > 0) check_forced_cells();
 	}
 
 };
 
 void print_markup(Board board, int row, int col) {
-	Cell markup = board.get_markup(row, col);
+	Cell markup = board.p_get_markup(row, col);
 	printf("markup (%d, %d) ", col, row);
 	for (int i = 0; i < grid_size + 1; i++) {
 		printf("%d ", markup.markup[i]);
@@ -188,11 +268,11 @@ void print_markup(Board board, int row, int col) {
 	printf(" %d - ", box);
 	for (int i = 0; i < grid_size; i++) {
 		//printf("%d ", markup.markup[i]);
-
 		printf(" %d ", board.boxes[box][i]);
 	}
 	printf("\n");
 }
+
 void print_all_markup(Board board) {
 	for (int i = 0; i < grid_size; i++) {
 		for (int j = 0; j < grid_size; j++) {
@@ -229,7 +309,6 @@ void printBoard(Board &board) {
 }
 
 void printBoard(Board &board, ofstream &output) {
-	//output << ",";
 	int digits = 0;
 	int mag = 1;
 	while (mag < grid_size) {
@@ -332,72 +411,22 @@ int valid_cell_placement(Board board, int row, int col, int n)
 	{
 		valid = 1;
 	}
-	//printf("valid? %d (%d, %d) %d %d %d\n", n, col, row, box_valid, col_valid, row_valid);
 	return valid;
 }
 
 Board check_valid_board(Board board) {
-	//int valid = 1;
 	board.valid = 1;
 	for (int i = 0; i < grid_size; i++) {
 		for (int j = 0; j < grid_size; j++) {
-			//int temp = valid_cell_placement(board, j, i, board.cells[i][j].value);
-			if (!valid_cell_placement(board, j, i, board.cells[i][j].value)) board.valid = -1; // valid = -1;
+			if (!valid_cell_placement(board, j, i, board.cells[i][j].value)) board.valid = -1; 
 		}
 	}
 	return board;
 }
 
-
-
-
-
-
-//
-//Cell getCell(Board &board, int id) {
-//	int col = id % grid_size;
-//	int row = id / grid_size;
-//
-//	return board.cells[col][row];
-//}
-//
-//Cell getCell(Board &board, int row, int col) {
-//	return board.cells[col][row];
-//}
-
 int getRandomValue(int min, int max) {
 
 	return rand() % max + min;
-}
-
-// Need this so we can safely put random numbers in the diagnol boxes
-// without needing to worry about row and column. Random numbers are important because 
-// we put random numbers in the diagnal when we call the fill remaining cells function which 
-// runs iteratively starting from cell 0,0 and ending on cell 8,8 it will adjust to these random placements
-// sort of like seeding with the rand function.
-// if we dont have this random placement then our first row will always be 1 2 3 4 5 6 7 8 9
-// we cant make the fill remaining cells function place random numbers in cells either because if we did
-// we could be repeating value placement attempts and it would significantly slow down our algorithm. 
-void fill_diagonal_boxes(Board &board)
-{
-	int box_grid_size = sqrt(grid_size);
-	int i, j, k;
-	for (i = 0; i < grid_size; i = i + box_grid_size)
-	{
-		int n;
-		for (j = 0; j < box_grid_size; j++)
-		{
-			for (k = 0; k < box_grid_size; k++)
-			{
-				do
-				{
-					n = rand() % grid_size + 1;
-				} while (!valid_cell_box(board, k + i, i + j, n));
-
-				board.cells[i + j][k + i].value = n;
-			}
-		}
-	}
 }
 
 void fill_diagonal_box(Board &board)
@@ -462,9 +491,6 @@ int solved = 0;
 
 int fill_remain_cells(Board &board, int newk)
 {
-
-
-
 	int i, j;
 
 	for (j = 0; j < grid_size; j++)
@@ -507,11 +533,48 @@ int fill_remain_cells(Board &board, int newk)
 	return 1;
 }
 
+Board parallel_fill_cells(Board board, int use_forced_cells)
+{
+	if (use_forced_cells) board.check_forced_cells();
+	int i, j, k;
 
+	for (j = 0; j < grid_size; j++)
+	{
+		for (i = 0; i < grid_size; i++)
+		{
+			if (board.cells[i][j].value == 0)
+			{
+				Cell cell = board.p_get_markup(j, i);
+				int markup_count = cell.markup[0];
+				for (k = 1; k <= markup_count; k++)
+				{
+					int value = board.get_kth(cell.markup, k);
+
+					board.add_value(value, j, i);
+					Board rBoard = parallel_fill_cells(board, use_forced_cells);
+					if (rBoard.valid == 1)
+					{
+						return rBoard;
+					}
+					else
+					{
+						board.remove_value(board.cells[i][j].value, j, i);
+						board.valid = -1;
+						if (omp_get_wtime() - t_ser > runtime_threshold) return board;
+					}
+				}
+				board.valid = -1;
+				return board;
+			}
+		}
+	}
+	board.valid = 1;
+	return board;
+}
 
 Board fill_cells_markup(Board board, int use_forced_cells)
 {
-	if (use_forced_cells)board.check_forced_cells();
+	if (use_forced_cells) board.check_forced_cells();
 	int i, j, k;
 	for (j = 0; j < grid_size; j++)
 	{
@@ -525,16 +588,10 @@ Board fill_cells_markup(Board board, int use_forced_cells)
 				{
 					int value = board.get_kth(cell.markup, k);
 
-					//if (valid_cell_placement(board, j, i, value))
-					//{
 					board.add_value(value, j, i);
-					//printBoard(board);
-					//print_all_markup(board);
-					//Board rBoard;// = board;
 					Board rBoard = fill_cells_markup(board, use_forced_cells);
 					if (rBoard.valid == 1)
 					{
-						//printBoard(rBoard);
 						return rBoard;
 					}
 					else
@@ -543,7 +600,6 @@ Board fill_cells_markup(Board board, int use_forced_cells)
 						board.valid = -1;
 						if (omp_get_wtime() - t_ser > runtime_threshold) return board;
 					}
-					//}
 				}
 				board.valid = -1;
 				return board;
@@ -554,13 +610,11 @@ Board fill_cells_markup(Board board, int use_forced_cells)
 	return board;
 }
 
-
 // Was thinking about just stopping mid way through the puzzle generation
 // but you have to completely fill in the board before you can validate
 // it, this is why instead I go back and randomly remove cell values.
 void fill_values(Board &board)
 {
-	//fill_diagonal_boxes(board);
 	fill_diagonal_box(board);
 	fill_remain_cells(board);
 }
@@ -609,151 +663,27 @@ void brute_force_parallel(Board board) {
 	}
 }
 
-
-void run_experiment() {
+void testingPremptive()
+{
 	ofstream output;
 	output.open("data.csv");
-	output << "runID, size, clue_count, board, alg1Time, alg1Solution, alg1Valid, alg2Time, alg2Solution, alg2Valid, alg3Time, alg3Solution, alg3Valid \n";
-	srand(omp_get_wtime());
-	Board board1, board2, board3, board4;
-	double elapsedTime;
-	int valid_solution;
-	for (int i = 0; i < experiment_ephochs; i++) {
-		solved = 0;
-
-		printf("run# %d", i + 1);
-		output << i + 1 << "," << grid_size << "," << clue_count << ",";
-		//srand(time(NULL));
-
-		Board board = generate_random_board(clue_count);
-		solved_board = board;
-		printBoard(board, output);
-
-
-		//brute force serial------------------------------------------------------------
-		board1 = board;
-
-		t_ser = omp_get_wtime();
-		fill_remain_cells(board1);
-		elapsedTime = omp_get_wtime() - t_ser;
-		output << elapsedTime << ",";
-
-
-		printBoard(board1, output);
-
-		valid_solution = check_valid_board(board1).valid;
-		output << valid_solution << ",";
-
-
-		////brute force parallel-------------------------------------------------------
-		//board2 = board;
-		////printBoard(board2, output);
-
-		//t_ser = omp_get_wtime();
-
-		//brute_force_parallel(board2);
-		//elapsedTime = omp_get_wtime() - t_ser;
-		//output << elapsedTime << ",";
-
-
-		//printBoard(solved_board, output);
-
-		//valid_solution = check_valid_board(solved_board).valid;
-		//if (!solved) valid_solution = -1;
-		//output << valid_solution << ",";
-
-		//markup serial------------------------------------------------------------
-		board3 = board;
-
-		t_ser = omp_get_wtime();
-		board3.generate_markup();
-		board3 = fill_cells_markup(board3, 0);
-		elapsedTime = omp_get_wtime() - t_ser;
-		output << elapsedTime << ",";
-
-
-		printBoard(board3, output);
-
-		valid_solution = check_valid_board(board3).valid;
-		output << valid_solution << ",";
-
-		//markup serial------------------------------------------------------------
-		board4 = board;
-
-		t_ser = omp_get_wtime();
-		board4.generate_markup();
-		board4 = fill_cells_markup(board4, 1);
-		elapsedTime = omp_get_wtime() - t_ser;
-		output << elapsedTime << ",";
-
-
-		printBoard(board4, output);
-
-		valid_solution = check_valid_board(board4).valid;
-		output << valid_solution << ",";
-
-		output << "\n";
-	}
-	output.close();
-}
-
-
-
-
-
-void testing() {
-
-	t_ser = omp_get_wtime();
-	srand(time(NULL));
-
-	Board board1 = generate_random_board(clue_count);
-
-
-	/*Board board1;
-	fill_values(board1);*/
-	printBoard(board1);
-	board1.generate_markup();
-	board1.check_forced_cells();
-	//print_all_markup(board1);
-	board1 = fill_cells_markup(board1, 0);
-	//int valid_board = ;
-	printf("valid? %d\n", check_valid_board(board1));
-	printBoard(board1);
-	//print_all_markup(board1);
-}
-
-int* res() {
-	//#pragma omp parallel
-	//	{
-	//		res();
-	//	}
-	int test[3]; // = { 8,9,3 };;
-	for (int i = 0; i < 3; i++) {
-		test[i] = i;
-	}
-	return test;
+    srand(omp_get_wtime());
+	Board board;
+	board = generate_random_board(clue_count);
+	solved_board = board;
+	printBoard(board);
+	board.generate_markup();
+	//board = fill_cells_markup(board, 0);
+	//printBoard(board);
+	print_all_markup(board);
+	//preemptive_sets(board);
 }
 
 int main()
 {
 	printf("-----------------------------------------test begins----------------------------------------------\n");
 
-	run_experiment();
-	//testing();
-
-	/*int* test = res();
-	for (int i = 0; i < 3; i++) {
-
-		printf("%d ", test[0 ]);
-	}*/
-
-
-	// 	omp_set_num_threads(8);
-	// #pragma omp parallel
-	// 	{
-	// 		int tid = omp_get_thread_num();
-	// 		printf("%d\n", tid);
-	// 	}
+	testingPremptive();
 
 	return 0;
 }
